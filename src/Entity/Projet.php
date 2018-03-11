06a1a8e9098ada20f182a479c6403afcbceb997c
unknown <lucie.g@codeur.online>
2018-03-11 17:13:39 +0100

    save of all vesoul's files

---
+++ src/Entity/Projet.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProjetRepository")
+ */
+class Projet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+     /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $image;
+    
+    /**
+     * @ORM\Column(type="url", length=100)
+     */
+    private $description;
+}
